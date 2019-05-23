302e0d215285c1a6a7735152c77083ea8f4973a6
Gil Oliveira <mariogil@edu.ulisboa.pt>
2019-05-23 20:08:19 +0100

    Some annotations

--- relayStations.py
+++ relayStations.py
@@ -15,11 +15,19 @@ from Graph import Graph
 
 
 # ---- PROGRAM START ----
+
+# Import the files names for the list of stations, the connection requests
+# and the output file from the terminal
 stationNetwork, stationPairs, outputFile = sys.argv[1:]
 
+# Reading the list of stations and storing the graph in stationsGraph
 stationsGraph = files.readStations(stationNetwork)
+
+# Reading the list of requests and storing the pairs in requestsLists
 requestsList = files.readRequests(stationPairs, stationsGraph)
 
+# Calculating the paths and times into resultsList
 resultsList = runRequests(stationsGraph, requestsList)
 
+# Writing the results to a file
 files.filesWriting(resultsList, outputFile)
